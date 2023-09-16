178b0daa174fd79eaf99e83edb234f39b273397a
Amanimakaveli <amanueldesalegn365@gmail.com>
2023-09-16 13:28:49 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase letters
+ * @c: number to be check
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c);
+{
+	if (c >= 55 && c <= 70)
+	{
+	return (1);
+	}
+	return (0);
+}
